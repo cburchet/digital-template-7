e46368f1c3a56e9ac9b7a827062975210b90f20c
cburchet <cburchet@gmu.edu>
2015-04-01 16:37:15 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ window.onload = function() {
         game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     }
     var player;
+    var cursors;
     
     var map;
     var backgroundLayer;
@@ -45,13 +46,14 @@ window.onload = function() {
         player = game.add.sprite(33, game.world.height - 49, 'dude');
 	 
     	game.physics.enable(player);
-		game.camera.follow(player);
+	game.camera.follow(player);
 	
-		player.body.bounce.y = 0.2;
-		player.body.gravity.y = 300;
-		
-		player.animations.add('left', [0, 1, 2, 3], 10, true);
-		player.animations.add('right', [5, 6, 7, 8], 10, true);
+	player.body.bounce.y = 0.2;
+	player.body.gravity.y = 300;
+	
+	player.animations.add('left', [0, 1, 2, 3], 10, true);
+	player.animations.add('right', [5, 6, 7, 8], 10, true);
+	cursors = game.input.keyboard.createCursorKeys();
     }
     
     function update() 
