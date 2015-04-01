0ae586f9b1909cae3529b0fdd126ff2ec5c4a073
cburchet <cburchet@gmu.edu>
2015-04-01 16:36:28 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,9 @@ window.onload = function() {
         // Load an image and call it 'logo'.
         game.load.tilemap('world', 'assets/world.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image( 'gameTiles', 'assets/tiles.png' );
+        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     }
+    var player;
     
     var map;
     var backgroundLayer;
@@ -39,10 +41,48 @@ window.onload = function() {
         
         backgroundLayer = map.createLayer('BackGroundLayer');
         backgroundLayer.resizeWorld();
+        
+        player = game.add.sprite(33, game.world.height - 49, 'dude');
+	 
+    	game.physics.enable(player);
+		game.camera.follow(player);
+	
+		player.body.bounce.y = 0.2;
+		player.body.gravity.y = 300;
+		
+		player.animations.add('left', [0, 1, 2, 3], 10, true);
+		player.animations.add('right', [5, 6, 7, 8], 10, true);
     }
     
     function update() 
     {
-
+        game.physics.arcade.collide(player, blockedLayer);
+    //	game.physics.arcade.collide(girl, blockedLayer);
+    //	game.physics.arcade.overlap(player, organ, collectOrgan, null, this);
+    //	game.physics.arcade.collide(player, girl, giveOrgan, null, this);
+        player.body.velocity.x = 0;
+	 
+    	if (cursors.left.isDown)
+    	{
+		player.body.velocity.x = -150;
+ 
+		player.animations.play('left');
+    	}
+    	else if (cursors.right.isDown)
+    	{
+    		player.body.velocity.x = 150;
+     		player.animations.play('right');
+    	}   
+    	else
+    	{
+    		player.animations.stop();
+    	    
+	    	player.frame = 4;
+    	}
+	
+    	if (cursors.up.isDown && player.body.onFloor())
+    	{
+    		player.body.velocity.y = -350;
+    	}
     }
 };
