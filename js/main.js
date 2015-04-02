4481311ae01119dd9b00544a6177adae55d2f52b
cburchet <cburchet@gmu.edu>
2015-04-01 21:59:52 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,13 +20,22 @@ window.onload = function() {
         game.load.tilemap('world', 'assets/world.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image( 'gameTiles', 'assets/tiles.png' );
         game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+        game.load.image('door', 'assets/castledoors.png');
     }
     var player;
     var cursors;
     
+    var door;
+    
+    var enemy;
+    var moveOn = false;
+    
     var map;
     var backgroundLayer;
     var blockedLayer;
+    
+    var level;
+    var levelText;
     
     function create() 
     {
@@ -53,15 +62,21 @@ window.onload = function() {
 	
 	player.animations.add('left', [0, 1, 2, 3], 10, true);
 	player.animations.add('right', [5, 6, 7, 8], 10, true);
+	
 	cursors = game.input.keyboard.createCursorKeys();
+	
+	door = game.add.sprite(game.world.width - 64, game.world.height - 128, 'door');
+	game.physics.enable(door);
+	
+	level = 1;
+	levelText = game.add.text(120, 0, 'Level: ' + level, { fontSize: '128px', fill: 'red' });
+	levelText.fixedToCamera = true;
     }
     
     function update() 
     {
         game.physics.arcade.collide(player, blockedLayer);
-    //	game.physics.arcade.collide(girl, blockedLayer);
-    //	game.physics.arcade.overlap(player, organ, collectOrgan, null, this);
-    //	game.physics.arcade.collide(player, girl, giveOrgan, null, this);
+	game.physics.arcade.overlap(player, door, nextLevel, null, this);
         player.body.velocity.x = 0;
 	 
     	if (cursors.left.isDown)
@@ -85,6 +100,22 @@ window.onload = function() {
     	if (cursors.up.isDown && player.body.onFloor())
     	{
     		player.body.velocity.y = -350;
+    	}
+    }
+    
+        function nextLevel()
+    {
+    	if (moveOn == true)
+    	{
+	    	level++;
+	    	levelText.text = 'Level: ' + level;
+	    	player.x = 33;
+	    	player.y = game.world.height-49;
+		player.body.velocity.x = 0;
+		player.animations.stop();
+		player.frame = 4;
+	    //	introText.visible = true;
+	    //	game.input.onDown.add(begin, this);
     	}
     }
 };
