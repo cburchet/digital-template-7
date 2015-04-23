b15c9e4003e333731798239b5e2b1b8da831aa41
cburchet <cburchet@gmu.edu>
2015-04-22 22:12:23 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -95,11 +95,11 @@ window.onload = function() {
        //enemy move/fire controls
        if(Phaser.Math.distance(player.x, player.y, goblin.x, goblin.y) > 10){
        	   game.physics.arcade.moveToXY(goblin, player.x, goblin.y, 50);
-       	   game.time.events.remove(enemyFireLoop);
+       	  // game.time.events.remove(enemyFireLoop);
        }
        else
        {
-       		enemyFireLoop = game.time.events.loop(Phaser.Timer.SECOND * 5, enemyfire, this);
+       	//	enemyFireLoop = game.time.events.loop(Phaser.Timer.SECOND * 5, enemyfire, this);
        }
        
        //player movement
